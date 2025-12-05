16da5593adf3c0dc67db9ad6a1ea85f8d1a10ab8
Marco Amorim <marco.amorim@seame.pt>
2025-12-05 15:56:00 +0000

    update gitignore

--- apps/Cluster/Dashboardproject/CMakeFiles/appDashboardproject.dir/appDashboardproject_autogen/EWIEGA46WW/qrc_qml.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/appDashboardproject.dir/appDashboardproject_autogen/EWIEGA46WW/qrc_qml.cpp.o: \
- /home/team2/Dashboardproject/appDashboardproject_autogen/EWIEGA46WW/qrc_qml.cpp \
- /usr/include/stdc-predef.h
