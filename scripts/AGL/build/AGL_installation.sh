ee1f500d075b243dd552e2919ee569b1488931fa
rmsa42 <rui.soares.almeida@outlook.pt>
2026-01-15 00:45:15 +0000

    Fixed AGL download path error

--- scripts/AGL/build/AGL_installation.sh
+++ scripts/AGL/build/AGL_installation.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-LOCAL_CONF=./conf/local.conf
-AGL_FEATURES="agl-app-framework agl-buildstats agl-devel agl-drm-lease agl-ic \
-	agl-netboot agl-package-management agl-pipewire agl-selinux"
-DEVEL_RECIPES=" cmake"
-QT_RECIPES=" qtbase qtmultimedia qtquick3d qtdeclarative"
-
-repo init -b trout -u https://gerrit.automotivelinux.org/gerrit/AGL/AGL-repo && \
-repo sync
+# Download AGL with repo command
+repo init -b trout -u https://gerrit.automotivelinux.org/gerrit/AGL/AGL-repo && repo sync
 
 # Init the build enviroment
 source meta-agl/scripts/aglsetup.sh -f -m raspberrypi5 -b build $AGL_FEATURES
@@ -16,15 +10,8 @@ source meta-agl/scripts/aglsetup.sh -f -m raspberrypi5 -b build $AGL_FEATURES
 bitbake-layers add-layer ../external/meta-qt6
 bitbake-layers add-layer ../meta-mylayer
 
-# Add recipes to local.conf
-echo "IMAGE_INSTALL:append = \"$QT_RECIPES\"" >> $LOCAL_CONF
-echo "IMAGE_INSTALL:append = \"$DEVEL_RECIPES\"" >> $LOCAL_CONF
-
-# Edit local.conf
-echo "SSTATE_DIR = \"../sstate-cache/\"" >> $LOCAL_CONF
-echo "DL_DIR = \"../downloads/\"" >> $LOCAL_CONF
-echo "INHERIT += \"rm_work\"" >> $LOCAL_CONF
-echo "RM_OLD_IMAGE = \"1\"" >> $LOCAL_CONF
+# Symlink the site.conf
+ln -sf $HOME/agl/site.conf $AGL_TOP/conf/.
 
 # Start creating the image and sdk
 time bitbake agl-image-weston
