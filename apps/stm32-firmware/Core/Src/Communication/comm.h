cc43221fd05ad8f6dcb359826675c7884e0dac01
hteixeir <hhzamith@gmail.com>
2026-01-20 21:36:31 +0000

    communication module documentation

--- apps/stm32-firmware/Core/Src/Communication/comm.h
+++ apps/stm32-firmware/Core/Src/Communication/comm.h
@@ -11,7 +11,15 @@
 
 #include "main.h"
 
-/* Communication Thread Entry Point */
+/**
+ * @brief Entry point for the communication thread.
+ *
+ * This function is the thread entry called by ThreadX/RTOS and runs the
+ * communication loop handling CAN bus interaction and message processing.
+ *
+ * @param thread_input Thread input parameter provided by the RTOS (typically a pointer or id). Can be NULL if unused.
+ * @return void
+ */
 void Communication_Thread_Entry(ULONG thread_input);
 
 #endif /* COMM_H */
