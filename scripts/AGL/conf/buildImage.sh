369f700d795cad297788c8b93988d63ceca8674e
rmsa42 <rui.soares.almeida@outlook.pt>
2026-02-02 12:35:43 +0000

    Integrated AGL docker into the Makefile

--- scripts/AGL/conf/buildImage.sh
+++ scripts/AGL/conf/buildImage.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+AGL_TOP=$HOME/$BUILD_DIR
+
 # Init the build enviroment
 source meta-agl/scripts/aglsetup.sh -f -m raspberrypi5 -b $BUILD_DIR $AGL_FEATURES
 
@@ -10,5 +12,4 @@ bitbake-layers add-layer ../meta-mylayer
 # Symlink the site.conf
 ln -sf $HOME/site.conf $AGL_TOP/conf/.
 
-bitbake rpi-config
-#time bitbake agl-image-weston
+time bitbake agl-image-weston
