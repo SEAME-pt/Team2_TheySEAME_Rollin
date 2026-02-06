9b6abed728acb5cc88e704cea1964f7883607e2f
rmsa42 <rui.soares.almeida@outlook.pt>
2026-02-06 16:21:14 +0000

    Added doxygen to Subejct and Observer

--- apps/rpi5-firmware/middleware/Observer.hpp
+++ apps/rpi5-firmware/middleware/Observer.hpp
@@ -2,9 +2,24 @@
 
 #include "Events.hpp"
 
+/**
+ * @class Observer
+ * @brief Observer interface
+ *
+ * The Observer interface defines the contract to receive events from a Subject
+ */
 class Observer {
 public:
 
 	virtual ~Observer() {};
+
+	/**
+	 * @brief Update after a notify
+	 *
+	 * Updates Observer accordingly the Event sent.
+	 * This function is called whenver a Subject calls notify
+	 *
+	 * @param event Describe the Event that triggered the notify
+	 */
 	virtual void update(Events event) = 0;
 };
