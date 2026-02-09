3ce9594bd108e16ad00687e270f08214f60e17dc
rmsa42 <rui.soares.almeida@outlook.pt>
2026-02-09 11:57:13 +0000

    Tested the event loop in RemoteControl

--- apps/rpi5-firmware/tests/unit/RemoteControlTests.cpp
+++ apps/rpi5-firmware/tests/unit/RemoteControlTests.cpp
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
 #include "RemoteControl.hpp"
+#include <cstring>
 #include "Mocks.h"
 
 using namespace testing;
@@ -18,8 +19,20 @@ protected:
  * ==========================================================================
  */
 TEST_F(RemoteControlTest, UpdateState) {
-	remote.setkey(KEY_A, 1);
-	EXPECT_EQ(remote.getkey(KEY_A), 1);
-	remote.setkey(KEY_A, 0);
-	EXPECT_EQ(remote.getkey(KEY_A), 0);
+	remote.setkey(JoyY, 1);
+	EXPECT_EQ(remote.getkey(JoyY), 1);
+	remote.setkey(JoyY, 0);
+	EXPECT_EQ(remote.getkey(JoyY), 0);
+}
+
+TEST_F(RemoteControlTest, EventLoop) {
+	struct input_event event;
+
+	std::memset(&event, 0, sizeof(input_event));
+	EXPECT_CALL(ev, pendingEvent())
+		.WillOnce(Return(1))
+		.WillOnce(Return(0));
+	EXPECT_CALL(ev, nextEvent())
+		.WillOnce(ReturnRef(event));
+	remote.getEvent();
 }
