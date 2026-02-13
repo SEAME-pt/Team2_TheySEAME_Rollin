ea32c396ebce8f44cd3ca8a54fbdb6b04f008009
rmsa42 <rui.soares.almeida@outlook.pt>
2026-02-13 13:45:16 +0000

    Removed unused tests and duplicate files

--- apps/rpi5-firmware/tests/unit/RemoteControlTests.cpp
+++ apps/rpi5-firmware/tests/unit/RemoteControlTests.cpp
@@ -11,13 +11,6 @@ protected:
 	RemoteControl remote{ev};
 };
 
-/*
- * @brief RemoteControl UpdateState test
- *
- * ====================== Requirement Traceability ===========================
- * [test->dsn~remote-control-read-inputs~1]
- * ==========================================================================
- */
 TEST_F(RemoteControlTest, UpdateState) {
 	remote.setkey(JoyY, 1);
 	EXPECT_EQ(remote.getkey(JoyY), 1);
