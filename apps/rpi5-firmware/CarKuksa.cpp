6b14bcd6a1b3e8b8369e7092583400b9de0b99c4
rmsa42 <rui.soares.almeida@outlook.pt>
2026-02-27 10:50:28 +0000

    Added CarKuksa class

---
+++ apps/rpi5-firmware/CarKuksa.cpp
@@ -0,0 +1,63 @@
+#include "CarKuksa.hpp"
+#include <iostream>
+
+CarKuksa::CarKuksa(RemoteControl &remote) : _subject(remote), _kuksa(kuksaLib) {
+	std::cout << "CarKuksa Constructor" << std::endl;
+}
+
+CarKuksa::~CarKuksa() {
+	std::cout << "CarKuska destructor" << std::endl;
+}
+
+int CarKuksa::processThrottle(const int rawThrottle) {
+	return ((rawThrottle - 127) / 1.27);
+}
+
+int CarKuksa::processSteering(const int rawSteering) {
+	return ((rawSteering - 127) / 127);
+}
+
+void CarKuksa::setThrottle(const int throttle) {
+	
+}
+
+void CarKuksa::setSteering(const int steering) {
+
+}
+
+void CarKuksa::setGear(const short gear) {
+
+}
+
+void CarKuksa::brake() {}
+
+void CarKuksa::update(Events event) {
+	std::cout << "Received notify" << std::endl;
+	switch (event) {
+		case Events::CAR_THROTTLE:
+			setThrottle(processThrottle(_subject.getkey(Keys::JoyY)));
+			break;
+		case Events::CAR_STEERING:
+			setSteering(processSteering(_subject.getkey(Keys::JoyZ)));
+			break;
+		case Events::CAR_BRAKE:
+			brake();
+			std::cout << "L2 Pressed" << std::endl;
+			break;
+		case Events::CAR_GEAR:
+			if (_subject.getkey(Keys::X)) {
+				setGear(NEUTRAL);
+			} else if (_subject.getkey(Keys::Y)) {
+				setGear(DRIVE);
+			} else if (_subject.getkey(Keys::A)) {
+				setGear(REVERSE);
+			} else if (_subject.getkey(Keys::B)) {
+				setGear(PARKING);
+			}
+			break;
+
+		default:
+			std::cout << "No event" << std::endl;
+			break;
+	}
+}
