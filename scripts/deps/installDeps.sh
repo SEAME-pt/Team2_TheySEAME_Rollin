cfde0d52bbe158a3b632a6bdf4d4847d1ca27d5d
rmsa42 <rui.soares.almeida@outlook.pt>
2026-03-03 12:25:40 +0000

    Fixed some errors in install script

--- scripts/deps/installDeps.sh
+++ scripts/deps/installDeps.sh
@@ -1,16 +1,18 @@
 # Install Protobuf 25.8
 git clone -b v25.8 --recurse-submodules git@github.com:protocolbuffers/protobuf.git
-mkdir build && cd build
+
+cd protobuf && mkdir -p build && cd build
 cmake ../ \
 	-DCMAKE_POSITION_INDEPENDENT_CODE=ON \
     -DCMAKE_BUILD_TYPE=Release \
     -Dprotobuf_BUILD_TESTS=OFF
 make -j 16
 make install
+cd ..
 
 # Install gRPC 
-git clone -b v1.60.1 --recurse-submodules --depth 1 https://github.com/grpc/grpc \
-cd grpc && mkdir build && cd build
+git clone -b v1.60.1 --recurse-submodules --depth 1 https://github.com/grpc/grpc
+cd grpc && mkdir -p build && cd build
 cmake .. \
        -DgRPC_INSTALL=ON \
        -DgRPC_BUILD_TESTS=OFF \
