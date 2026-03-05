aba87c53f6b39249e52f0adb52cf8cc455bc74bf
Marco Amorim <marco.amorim@seame.pt>
2026-03-05 10:50:44 +0000

    Refactor main.cpp: clean up unused variables and comments for improved readability

--- apps/carla/PiracerSim/src/main.cpp
+++ apps/carla/PiracerSim/src/main.cpp
@@ -26,10 +26,8 @@ namespace cc = carla::client;
 namespace cg = carla::geom;
 namespace csd = carla::sensor::data;
 
-// Variável global para o veículo
 boost::shared_ptr<cc::Vehicle> g_vehicle;
 
-// Handler para SIGINT (Ctrl+C)
 void handle_sigint(int) {
   if (g_vehicle) {
     std::cout << "\nSIGINT received: destroying vehicle..." << std::endl;
@@ -54,14 +52,12 @@ int main() {
     kuksaLib kuksaCtrl;
     std::thread kuksaThread([&](){kuksaCtrl.subscribeFromKuksa();});
     kuksaThread.detach();
-    
+
     kuksaCtrl.sendValueToKuksa("Vehicle.Speed", 0.1f);
     cc::Client client("localhost", 2000);
-    // client.SetTimeout(40s);
 
     auto world = client.GetWorld();
 
-    // Synchronous mode (very important)
     auto settings = world.GetSettings();
     settings.synchronous_mode = true;
     settings.fixed_delta_seconds = piracer::WORLD_DT_S;
@@ -82,34 +78,9 @@ int main() {
     auto vehicle = boost::static_pointer_cast<cc::Vehicle>(actor);
     g_vehicle = vehicle;
 
-    // Basic physical adjustment (mass; you can also modify wheels)
     auto phys = vehicle->GetPhysicsControl();
 
     vehicle->ApplyPhysicsControl(phys);
-
-    // // RGB camera setup
-    // auto cam_bp_opt = blueprints->Find("sensor.camera.rgb");
-    // if (!cam_bp_opt) { std::cerr << "camera bp not found\n"; return 1; }
-    // carla::actors::ActorBlueprint cam_bp = *cam_bp_opt;
-
-    // cam_bp.SetAttribute("image_size_x", "640");
-    // cam_bp.SetAttribute("image_size_y", "480");
-    // cam_bp.SetAttribute("fov", "90");
-    // cam_bp.SetAttribute("sensor_tick", std::to_string(piracer::CAM_TICK_S));
-
-    // cg::Transform cam_tf(
-    //   cg::Location(piracer::CAM_X, piracer::CAM_Y, piracer::CAM_Z),
-    //   cg::Rotation(piracer::CAM_PITCH_DEG, 0.0, 0.0)
-    // );
-
-    // auto cam_actor = world.SpawnActor(cam_bp, cam_tf, vehicle.get());
-    // auto cam = boost::static_pointer_cast<cc::Sensor>(cam_actor);
-
-    // cam->Listen([](auto data) {
-    //   auto img = boost::static_pointer_cast<csd::Image>(data);
-    //   std::lock_guard<std::mutex> lk(g_mtx);
-    //   g_last_image = img;
-    // });
 
     carla::rpc::VehicleControl carlaCtrl;
     float cur_throttle = 0.0f;
@@ -117,7 +88,7 @@ int main() {
 
     while (1)
     {
-      // 1) advance simulation
+      // advance simulation
       world.Tick(10s);
       static carla::geom::Location smooth_loc = vehicle->GetTransform().location;
 
@@ -129,8 +100,7 @@ int main() {
       target.y -= fwd.y * 6.0f;
       target.z += 2.5f;
 
-      // smoothing
-      float alpha = 0.1f; // menor = mais suave
+      float alpha = 0.1f;
       smooth_loc.x = (1 - alpha) * smooth_loc.x + alpha * target.x;
       smooth_loc.y = (1 - alpha) * smooth_loc.y + alpha * target.y;
       smooth_loc.z = (1 - alpha) * smooth_loc.z + alpha * target.z;
@@ -139,21 +109,10 @@ int main() {
       cam_rot.pitch = -12.0f;
 
       world.GetSpectator()->SetTransform({smooth_loc, cam_rot});
-      // // 2) read the latest image (if needed)
-      // {
-      //   std::lock_guard<std::mutex> lk(g_mtx);
-      //   if (g_last_image) {
-      //     // Here we would call our ADAS with the image
-      //     // (*g_last_image)->GetWidth(), GetHeight(), raw data, etc.
-      //   }
-      // }
 
-      // 3) placeholder: target command from your ADAS
+      // placeholder: target command from our ADAS
       float target_throttle = kuksaCtrl.getThrottle() / 100.0f;
       float target_steer = kuksaCtrl.getSteering() / 5.0f;
-
-      // cur_throttle = Slew(cur_throttle, target_throttle, (float)piracer::MAX_THROTTLE_SLEW_PER_TICK);
-      // cur_steer    = Slew(cur_steer,    target_steer,    (float)piracer::MAX_STEER_SLEW_PER_TICK);
       
       carlaCtrl.throttle = std::clamp(target_throttle, 0.0f, 1.0f);
       carlaCtrl.steer = std::clamp(target_steer, -1.0f, 1.0f);
@@ -195,11 +154,6 @@ int main() {
             << ", gear=" << carlaCtrl.gear
             << std::endl;
     }
-
-    // Stop and destroy camera and vehicle actors
-    // cam->Stop();
-    // cam->Destroy();
-    // vehicle->Destroy();
   } catch (const cc::TimeoutException &e) {
     std::cout << '\n' << e.what() << std::endl;
     return 1;
