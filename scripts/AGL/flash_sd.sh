0bfca0d71ec3c30c436e5e40c5d190a6ea4ac6d0
rmsa42 <rui.soares.almeida@outlook.pt>
2026-05-19 14:41:57 +0100

    Grow rootfs. Added sanitizers recipe

--- scripts/AGL/flash_sd.sh
+++ scripts/AGL/flash_sd.sh
@@ -2,9 +2,9 @@
 
 set -x
 
-IMAGE_NAME=$(find $HOME/Documents/Repo_Joao/scripts/AGL/agl/tmp/deploy/images -name "*rootfs.wic.xz")
+IMAGE_NAME=$(find $PWD/agl/tmp/deploy/images -name "*rootfs.wic.xz")
 
 sudo umount /dev/sda1
 sudo umount /dev/sda2
-xzcat ${IMAGE_NAME} | sudo dd of=/dev/sda bs=4M
+xzcat ${IMAGE_NAME} | sudo dd of=/dev/sda bs=4M status=progress
 sync
